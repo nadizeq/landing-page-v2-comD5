b55f975ea0f40d8f4dbd50be9e6f1ba31676d2fb
Nadiah Zairee <nadizeq@yahoo.com>
2022-02-25 07:10:16 +0000

    Finish 90% first page and add new assets

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -2,12 +2,22 @@ import React from "react";
 import "../../App";
 //import { Button } from "../button/Button";
 import "./HeroSection.css";
+import comD5 from "../../image/Company-logo.png";
+import rectangle from "../../image/highlight.png";
+import pc from "../../image/pc.png";
+import phone from "../../image/mobile-comd5.png";
 
 function HeroSection() {
   return (
     <div className="hero-container">
+      <button className="login-btn">Login</button>
+      <img className="company-logo" src={comD5} alt="ComD5 logo" />
+      <img className="pc-comd5 " src={pc} alt="Graph on ComD5 platform" />
+      <img className="phone-comd5" src={phone} alt="Graph on ComD5 platform" />
       <h1>
-        Your Navigator in <br /> the World of <br /> Commodity Trade
+        Your <img className="rectangle-size" src={rectangle} alt />
+        <div className="text-on-image">Navigator</div> in <br /> the World of{" "}
+        <br /> Commodity Trade
       </h1>
 
       <p>
