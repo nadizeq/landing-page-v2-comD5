d4d638401b27e550449d8c76eff7ec2ac549e3df
Nadiah Zairee <nadizeq@yahoo.com>
2022-03-02 02:42:25 +0000

    Footer, 80% responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import { IoCallOutline } from "react-icons/io5";
 import { AiOutlineMail } from "react-icons/ai";
 import { AiFillChrome } from "react-icons/ai";
 import { AiOutlineChrome } from "react-icons/ai";
-import WebsiteIcon from "../../image/websiteIcon.png";
+import ComD5White from "../../image/CompanyLogo/comD5-white.png";
 
 function Footer() {
   let iconStyles = { color: "white", fontSize: "1.8em" };
@@ -14,8 +14,10 @@ function Footer() {
         <div className="footer-link-wrapper">
           <div class="footer-link-items">
             <h2>Contact Us</h2>
-            <IoCallOutline style={iconStyles} />
-            <text className="contact-number"> +60123456789</text>
+            <text className="contact-number">
+              <IoCallOutline style={iconStyles} />
+              +60123456789
+            </text>
             <div></div>
             <AiOutlineMail style={iconStyles} />
             <text className="email-address">
@@ -25,6 +27,12 @@ function Footer() {
             <AiOutlineChrome style={iconStyles} />
 
             <text className="url-link">www.comd5.com.my</text>
+          </div>
+        </div>
+
+        <div className="footer-link-wrapper">
+          <div class="footer-link-items">
+            <img className="comD5-logo-white" src={ComD5White} />
           </div>
         </div>
       </div>
