7b0a64978250ac9e606b88b82617082f8747e600
Rusydiah <rsydh.slmn@gmail.com>
2022-03-02 10:38:43 +0800

    asset page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../App.css";
+import Asset from "../Assetpage/Asset";
 import HeroSection from "../HeroSection/HeroSection";
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Asset />
     </>
   );
 }
