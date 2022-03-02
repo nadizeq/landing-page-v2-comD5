164208f17982c43dc66486929d7279c8ce3adfbb
Nadiah Zairee <nadizeq@yahoo.com>
2022-03-02 03:30:06 +0000

    fix console error

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,28 +10,28 @@ function Footer() {
   let iconStyles = { color: "white", fontSize: "1.8em" };
   return (
     <div className="footer-container">
-      <div class="footer-links">
+      <div className="footer-links">
         <div className="footer-link-wrapper">
-          <div class="footer-link-items">
+          <div className="footer-link-items">
             <h2>Contact Us</h2>
-            <text className="contact-number">
+            <div className="contact-number">
               <IoCallOutline style={iconStyles} />
               +60123456789
-            </text>
+            </div>
             <div></div>
             <AiOutlineMail style={iconStyles} />
-            <text className="email-address">
+            <div className="email-address">
               syarusnadvishaimyaz@besquare.com.my
-            </text>
+            </div>
             <div></div>
             <AiOutlineChrome style={iconStyles} />
 
-            <text className="url-link">www.comd5.com.my</text>
+            <div className="url-link">www.comd5.com.my</div>
           </div>
         </div>
 
         <div className="footer-link-wrapper">
-          <div class="footer-link-items">
+          <div className="footer-link-items">
             <img className="comD5-logo-white" src={ComD5White} />
           </div>
         </div>
